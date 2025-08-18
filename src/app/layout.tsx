caeeda5a82ee087f493181f1c97f140cd10fdad0
ritikrajput696 <rixusinput696@gmail.com>
2025-08-18 23:30:31 +0530

    fixed pull to refresh bug,

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       </Head> */}
 
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} overscroll-none antialiased`}
       >
         {children}
       </body>
