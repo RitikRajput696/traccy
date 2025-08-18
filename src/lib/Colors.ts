fc5c4924741fe9e063938513cba157cf94e1fbb7
ritikrajput696 <rixusinput696@gmail.com>
2025-08-19 00:07:21 +0530

    added black shape color

--- src/lib/Colors.ts
+++ src/lib/Colors.ts
@@ -1,4 +1,4 @@
 export const COLORS = {
   canvasColor: "#fff", //solid white
-  panelColors: ["#fbe7c6", "#b4f8c8", " #a0e7e5", "#ffaebc"], // yellow, mint, triffiny blue, pink
+  panelColors: ["#000000", "#fbe7c6", "#b4f8c8", " #a0e7e5", "#ffaebc"], // yellow, mint, triffiny blue, pink
 };
